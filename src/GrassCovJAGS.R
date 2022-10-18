7602580d2562b44862acb98db3efb7a554bbb91c
georgewoolsey <george.woolsey@colostate.edu>
2022-10-18 14:35:03 -0600

    updating

--- src/GrassCovJAGS.R
+++ src/GrassCovJAGS.R
@@ -12,8 +12,8 @@
       # deterministic model
       mu[i] <- ilogit(
          b0
-         + b1*x_mtrx[i,2]
-         + b2*x_mtrx[i,3]                   
+         + b1*x_mtrx[i,2] 
+         + b2*x_mtrx[i,3] 
          + b3*x_mtrx[i,4]
          + b4*x_mtrx[i,5]
       )
@@ -21,19 +21,24 @@
       a[i] <- (mu[i]^2 - mu[i]^3 - mu[i]*sigma^2) / sigma^2
       b[i] <- (mu[i] - 2*mu[i]^2 + mu[i]^3 - sigma^2 + mu[i]*sigma^2) / sigma^2
       # likelihood
-        # returns density (for continuous) because l.h.s. is data (deterministic b/c defined in data) 
         y[i] ~ dbeta(a[i], b[i]) 
-      # posterior predictive distribution of y.new (for model checking)
-        # returns random number generator because l.h.s. is not data (i.e. it is unknown/stochastic node)
         y_sim[i]  ~ dbeta(a[i], b[i]) 
     }
     # Derived quantities
-    #posterior predictive checks
-      mean_y = mean(y)
-      sd_y = sd(y)
-      mean_y_sim = mean(y_sim)
-      sd_y_sim = sd(y_sim)
-      p_val_mean = step(mean_y_sim - mean_y)
-      p_val_sd = step(sd_y_sim - sd_y)
+      #posterior predictive checks
+        mean_y = mean(y)
+        sd_y = sd(y)
+        mean_y_sim = mean(y_sim)
+        sd_y_sim = sd(y_sim)
+        p_val_mean = step(mean_y_sim - mean_y)
+        p_val_sd = step(sd_y_sim - sd_y)
+      # sum of squares 
+        for(j in 1:n) {
+          sq[j] <- (y[j]-mu[j])^2
+          sq_sim[j] <- (y_sim[j]-mu[j])^2
+        }
+        fit_y <- sum(sq)
+        fit_y_sim <- sum(sq_sim)
+        p_val_fit = step(fit_y_sim - fit_y)
   }
   
